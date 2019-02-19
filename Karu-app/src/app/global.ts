98421943d0d50e30f121e0f28e4212ed16f32f35
xixolio <ignacio.araya.11@alumnos.usm.cl>
2019-02-19 11:15:00 -0300

    asdasd

--- Karu-app/src/app/global.ts
+++ Karu-app/src/app/global.ts
@@ -3,7 +3,7 @@ export const GlobalVariable = Object.freeze({
 	 //MIDDLE_API_URL: 'http://localhost:9090/http://127.0.0.1:8008/',
 	 //BASE_API_URL: 'http://localhost:9090/http://192.168.0.200:8000/',
 	 //MIDDLE_API_URL: 'http://localhost:9090/http://192.168.0.210:8000/',
-	 BASE_API_URL: '192.168.0.200:9090/http://192.168.0.200:8000/',
-	 MIDDLE_API_URL: '192.168.0.200:9090/http://192.168.0.210:8000/',
+	 BASE_API_URL: 'http://192.168.0.200:9090/http://192.168.0.200:8000/',
+	 MIDDLE_API_URL: 'http://192.168.0.200:9090/http://192.168.0.210:8000/',
 	 
  });
